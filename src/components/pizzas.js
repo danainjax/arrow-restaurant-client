ad374d8e89d200ce2b10c29560271c62fcddf93e
danainjax <danainjax@gmail.com>
2021-06-25 17:53:50 -0400

    got pizzas to display in grid

--- src/components/pizzas.js
+++ src/components/pizzas.js
@@ -20,14 +20,25 @@ class Pizzas {
     
     render() {
         const pizzaMenu = document.getElementById('pizza-menu')
-        let pizzasCard = this.pizzas.map(pizza=> `<div class="row no-gutters">
-        <div class="card col-sm-6 col-lg-3">
-            <div class="card-img-top"><img src=${pizza.image_url} /></div>
-            <div class="card-body">
-                <p class="card-text">${pizza.name} - ${pizza.description} </div>
-            <div class="card-footer">${pizza.price}</div>
+        let pizzasCard = this.pizzas.map(pizza=>   
+        `
+            <div class="col mb-6">
+                <div class="card">
+                    <img src=${pizza.image_url} class="card-img-top" alt="pizza menu item">
+                    <div class="card-body">
+                        <h5 class="card-title">${pizza.name}</h5>
+                        <p class="card-text">${pizza.description}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">${pizza.price}</small>
+                        <a id="order-bttn" class="btn btn-primary btn-lg" href="#" role="button" position="right">Order</a>
+                    </div>
+                 </div>   
+            </div>
         </div>`).join('')
+        
         pizzaMenu.innerHTML = pizzasCard
+        console.log(pizzasCard)
         // pizzaMenu.innerHTML = this.pizzas.map(pizza=> `<li>${pizza.name}, ${pizza.description}, ${pizza.price}</li>`).join('')
         console.log('my pizzas are', this.pizzas)
     }

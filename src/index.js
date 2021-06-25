6082d9193c3e7efa9ee4ac3c2b34b587a92719d2
danainjax <danainjax@gmail.com>
2021-06-25 09:05:58 -0400

    updated click action on opener

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@
 
 const bttn = document.getElementById('menu-bttn');
 bttn.addEventListener('click', () => {
-    const app = new App()
-
+    const app = new App();
+    opener = document.getElementById('opener')
+    opener.innerHTML = " "
+    opener.outerHTML = " "
+    
 
 })
 
